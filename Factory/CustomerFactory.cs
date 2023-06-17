fc9c1a1f0ba652255b12e1de2b3e737c66e9c6bf
William Thaddeus <88379454+Williamthadd@users.noreply.github.com>
2023-06-17 21:06:24 +0700

    Add files via upload

---
+++ Factory/CustomerFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PSDLEC.Model;
+
+namespace PSDLEC.Factory
+{
+    public class CustomerFactory
+    {
+        public static Customer CreateCustomer(string customername, string customeremail, string customeraddress, string customerpassword, string customerphone)
+        {
+            return new Customer
+            {
+                CustomerName = customername,
+                CustomerEmail = customeremail,
+                CustomerAddress = customeraddress,
+                CustomerPassword = customerpassword,
+                CustomerPhone = customerphone,
+                CustomerRole = "Customer"
+            };
+        }
+
+    }
+}
---
+++ Factory/TransactionFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PSDLEC.Model;
+
+namespace PSDLEC.Factory
+{
+    public class TransactionFactory
+    {
+        public static Transaction CreateTransaction(DateTime transactiondate, string paymentmethod, string paymentnumber, int customerid, int productid, string uploadfile)
+        {
+            return new Transaction
+            {
+                TransactionDate = transactiondate,
+                PaymentMethod = paymentmethod,
+                PaymentNumber = paymentnumber,
+                CustomerID = customerid,
+                ProductID = productid,
+                UploadFile = uploadfile
+            };
+        }
+    }
+}
