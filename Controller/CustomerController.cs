ede2d84a383b19eb8516b34d36e97ea08314000f
William Thaddeus <88379454+Williamthadd@users.noreply.github.com>
2023-06-17 21:05:52 +0700

    Add files via upload

---
+++ Controller/CustomerController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PSDLEC.Model;
+using PSDLEC.Repository;
+
+namespace PSDLEC.Controller
+{
+    public class CustomerController
+    {
+        CustomerRepository customerrepo = new CustomerRepository();
+
+        public string CheckName(string name)
+        {
+            string response = "";
+
+            if (name.Equals("")) response = "Name cannot be empty!";
+            else if (name.Length < 5 || name.Length > 50) response = "Name must be between 5 and 50 characters!";
+
+            return response;
+        }
+
+
+        public string CheckEmail(string email)
+        {
+            string response = "";
+            if (email.Equals("")) response = "Email cannot be empty!";
+            else if (customerrepo.GetEmail(email) != null) response = "Must use new email!";
+
+            return response;
+        }
+
+        public string CheckAddress(string address)
+        {
+            string response = "";
+            if (address.Equals("")) response = "Address must be filed!";
+
+            return response;
+        }
+
+        public string CheckPassword(string password)
+        {
+            string response = "";
+            bool containsAlphabetic = password.Any(char.IsLetter);
+            bool containsNumeric = password.Any(char.IsDigit);
+
+            if (password.Equals("")) response = "Password must be filled!";
+            else if (!containsNumeric) response = "Password must be alphanumeric";
+            else if (!containsAlphabetic) response = "Password must be alphanumeric";
+
+            return response;
+        }
+
+        public string CheckPhone(string phone)
+        {
+            string response = "";
+
+            if (phone.Equals("")) response = "Phone number must be filled!";
+            else if (customerrepo.GetPhone(phone) != null) response = "Must use new phone number!";
+
+            return response;
+        }
+
+        public string CheckLogin(string email, string password)
+        {
+            Customer customer = customerrepo.login(email, password);
+
+            string response = "";
+
+            if (customer == null) response = "Email or password is incorrect";
+            else if (email.Equals("") || password.Equals("")) response = "Email and password cannot be empty";
+
+            return response;
+
+        }
+
+    }
+}
---
+++ Controller/TransactionController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PSDLEC.Model;
+using PSDLEC.Repository;
+
+namespace PSDLEC.Controller
+{
+    public class TransactionController
+    {
+        public string CheckPaymentMethod(string payment)
+        {
+            string response = "";
+
+            if (payment.Equals("")) response = "Payment method must be selected!";
+
+            return response;
+        }
+
+        public string CheckPaymentNumber(string number)
+        {
+            string response = "";
+
+            if (number.Equals("")) response = "Payment number must be selected!";
+
+            return response;
+        }
+
+        public string CheckFile(string file)
+        {
+            string response = "";
+
+            if (file.Equals("")) response = "File must be selected!";
+            else if (file.Contains(".zip") == false)
+            {
+                response = "File must be in zip";
+            }
+
+            return response;
+        }
+
+    }
+}
