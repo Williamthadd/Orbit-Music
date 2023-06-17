a0e5d99fc7e63e91098a06672dea4223baf9ae12
William Thaddeus <88379454+Williamthadd@users.noreply.github.com>
2023-06-17 21:07:04 +0700

    Add files via upload

---
+++ Repository/CustomerRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PSDLEC.Model;
+using PSDLEC.Factory;
+
+namespace PSDLEC.Repository
+{
+    public class CustomerRepository
+    {
+        Database1Entities2 db = new Database1Entities2();
+
+        public Customer GetCustomer(int customerid)
+        {
+            Customer customer = db.Customer.Where(x => x.CustomerID == customerid).FirstOrDefault();
+            return customer;
+        }
+
+        public Customer GetEmail(string email)
+        {
+            Customer emailcheck = db.Customer.Where(x => x.CustomerEmail == email).FirstOrDefault();
+            return emailcheck;
+        }
+
+        public Customer GetPhone(string phone)
+        {
+            Customer phonecheck = db.Customer.Where(x => x.CustomerPhone == phone).FirstOrDefault();
+            return phonecheck;
+        }
+
+        public void Register(string customername, string customeremail, string customeraddress, string customerpassword, string customerphone)
+        {
+            Customer customer = CustomerFactory.CreateCustomer(customername, customeremail, customeraddress, customerpassword, customerphone);
+            db.Customer.Add(customer);
+            db.SaveChanges();
+        }
+
+        public Customer login(string customeremail, string customerpassword)
+        {
+            Customer customer = db.Customer.Where(x => x.CustomerEmail == customeremail && x.CustomerPassword == customerpassword).FirstOrDefault();
+            return customer;
+        }
+
+        public void ProfileUpdate(int customerid, string customername, string customeremail, string customeraddress, string customerpassword, string customerphone)
+        {
+            Customer customer = db.Customer.Where(x => x.CustomerID == customerid).FirstOrDefault();
+            customer.CustomerName = customername;
+            customer.CustomerEmail = customeremail;
+            customer.CustomerAddress = customeraddress;
+            customer.CustomerPassword = customerpassword;
+            customer.CustomerPhone = customerphone;
+            db.SaveChanges();
+        }
+
+    }
+}
---
+++ Repository/TransactionRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PSDLEC.Model;
+using PSDLEC.Factory;
+
+namespace PSDLEC.Repository
+{
+    public class TransactionRepository
+    {
+        Database1Entities2 db = new Database1Entities2();
+
+        public void addTransaction(DateTime transactiondate, string paymentmethod, string paymentnumber, int customerid, int productid, string uploadfile)
+        {
+            db.Transaction.Add(TransactionFactory.CreateTransaction(transactiondate, paymentmethod, paymentnumber, customerid, productid, uploadfile));
+            db.SaveChanges();
+        }
+
+        public List<Transaction> trlist()
+        {
+            List<Transaction> transaction = (from row in db.Transaction select row).ToList();
+            return transaction;
+        }
+
+    }
+}
